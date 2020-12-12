a735a560aecfab85cc4b0416d708df81d76341db
m6ttw <matthewrhyswilliams@live.com>
2020-12-12 15:29:40 +0000

    minor changes to syntax and spelling

--- console.py
+++ console.py
@@ -1,5 +1,3 @@
-import pdb
-
 from models.merchant import Merchant
 import repositories.merchant_repository as merchant_repository
 
@@ -12,30 +10,23 @@ import repositories.transaction_repository as transaction_repository
 
 merchant_1 = Merchant("Asda")
 merchant_repository.save(merchant_1)
-merchant_2 = Merchant("Amazon")
+merchant_2 = Merchant("Vodafone")
 merchant_repository.save(merchant_2)
 merchant_3 = Merchant("Adidas")
 merchant_repository.save(merchant_3)
 
-tag_1 = Tag("Finances")
+tag_1 = Tag("Bills")
 tag_repository.save(tag_1)
-tag_2 = Tag("Charity")
+tag_2 = Tag("Groceries")
 tag_repository.save(tag_2)
-tag_3 = Tag("Entertainment")
+tag_3 = Tag("Shopping")
 tag_repository.save(tag_3)
-tag_4 = Tag("Groceries")
-tag_repository.save(tag_4)
-tag_5 = Tag("Misc")
-tag_repository.save(tag_5)
-tag_6 = Tag("Shopping")
-tag_repository.save(tag_6)
-tag_7 = Tag("Transport")
-tag_repository.save(tag_7)
 
-transaction_1 = Transaction(50, merchant_1, tag_4)
+transaction_1 = Transaction(50, merchant_1, tag_2)
 transaction_repository.save(transaction_1)
-transaction_2 = Transaction(65, merchant_3, tag_6)
+transaction_2 = Transaction(65, merchant_3, tag_3)
 transaction_repository.save(transaction_2)
+transaction_3 = Transaction(20, merchant_2, tag_1)
 
 
 merchant_repository.select_all()
@@ -46,15 +37,9 @@ transaction_repository.select_all()
 merchant_1.name = "Tesco"
 merchant_repository.update(merchant_1)
 
-tag_1.category = "Bills"
+tag_1.category = "Finances"
 tag_repository.update(tag_1)
-
-transaction_1.amount = 40
-transaction_repository.update(transaction_1)
-
 
 merchant_repository.delete_all()
 tag_repository.delete_all()
 transaction_repository.delete_all()
-
-pdb.set_trace()
