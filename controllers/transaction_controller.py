0286b475a0129f0dccfd19a5cdf6e349153c484e
m6ttw <matthewrhyswilliams@live.com>
2020-12-13 12:41:37 +0000

    merchants and tags complete, add transaction amount and total still to do

---
+++ controllers/transaction_controller.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.transaction import Transaction
+import repositories.transaction_repository as transaction_repository
+import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
+
+transactions_blueprint = Blueprint("transactions", __name__)
+
+@transactions_blueprint.route("/transactions")
+def transactions():
+    transactions = transaction_repository.select_all()
+    return render_template("transactions/index.html", transactions=transactions)
+
+
+@transactions_blueprint.route("/transactions/new")
+def new_transaction():
+    merchants = merchant_repository.select_all()
+    tags = tag_repository.select_all()
+    return render_template("transactions/new.html", merchants=merchants, tags=tags)
+
+
+@transactions_blueprint.route("/transactions", methods=["POST"])
+def create_transaction():
+    amount = request.form["amount"]
+    merchant_id = request.form["merchant_id"]
+    tag_id = request.form["tag_id"]
+    merchant = merchant_repository.select(merchant_id)
+    tag = tag_repository.select(tag_id)
+    new_transaction = Transaction(amount, merchant, tag)
+    transaction_repository.save(new_transaction)
+    return redirect("/transactions")
