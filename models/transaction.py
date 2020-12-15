05f40c7d3bc045b7ce8b7b30d25b2f3fe9470ff2
m6ttw <matthewrhyswilliams@live.com>
2020-12-15 11:59:38 +0000

    some background colour css

--- models/transaction.py
+++ models/transaction.py
@@ -1,8 +1,7 @@
 class Transaction():
 
-    def __init__(self, amount, merchant, tag, time_created, id=None):
+    def __init__(self, amount, merchant, tag, id=None):
         self.amount = amount
         self.merchant = merchant
         self.tag = tag
-        self.time_created = time_created
         self.id = id
